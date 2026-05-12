f53030fe5366ebf49ae1cecb9f4dfc7ac4a0f2cd
amirhossein-beh <amirhosseinbehnoosh@gmail.com>
2026-05-12 11:34:39 +0330

    add authentication

---
+++ src/main/java/com/pedasco/apex/security/JwtUtil.java
@@ -0,0 +1,66 @@
+package com.pedasco.apex.security;
+
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+    @Value("${apex.jwt.secret}")
+    private String secret;
+
+
+    @Value("${apex.jwt.expiration-ms}")
+    private long expirationMs;
+
+
+    // generate encrypt key
+    private SecretKey getSigningKey(){
+        return Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8));
+    }
+
+
+    // generate token with username and role
+    public String generateToken(String username , String role){
+        return Jwts.builder()
+                .subject(username)
+                .claim("role" , role)
+                .issuedAt(new Date())
+                .expiration(new Date(System.currentTimeMillis() + expirationMs))
+                .signWith(getSigningKey())
+                .compact();
+    }
+
+
+    private Claims extractAllClaims(String token){
+        return Jwts.parser()
+                .verifyWith(getSigningKey())
+                .build()
+                .parseEncryptedClaims(token)
+                .getPayload();
+    }
+
+    public String getUsernameFromToken(String token) {
+        return extractAllClaims(token).getSubject();
+    }
+    public String getRoleFromToken(String token) {
+        return extractAllClaims(token).get("role", String.class);
+    }
+    // token valid ? or expired?
+    public boolean validateToken(String token) {
+        try {
+            Claims claims = extractAllClaims(token);
+            return !claims.getExpiration().before(new Date());
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+}
