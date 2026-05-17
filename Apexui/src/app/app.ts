0e864559ed94c90566f1914f503f6764a9d8629b
amirhossein-beh <amirhosseinbehnoosh@gmail.com>
2026-05-17 13:49:15 +0330

    adding front-end

---
+++ Apexui/src/app/app.ts
@@ -0,0 +1,13 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import {Button} from 'primeng/button';
+
+@Component({
+  selector: 'app-root',
+  imports: [Button],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  protected readonly title = signal('Apexui');
+}
